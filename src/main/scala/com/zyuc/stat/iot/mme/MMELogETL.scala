6a2fe89234bd9e4e05bbe76da1c0a872ff1810f3
slview <cqfrog@163.com>
2017-07-20 20:37:52 +0800

    Merge branch 'master' of https://github.com/zcw5116/IOTProject
    # Conflicts:
    #	src/main/scala/com/zyuc/stat/iot/mme/MMEAnalysis.scala

--- src/main/scala/com/zyuc/stat/iot/mme/MMELogETL.scala
+++ src/main/scala/com/zyuc/stat/iot/mme/MMELogETL.scala
@@ -1,35 +1,52 @@
 package com.zyuc.stat.iot.mme
 
-import java.util.Date
-
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{Logging, SparkConf, SparkContext}
-import com.zyuc.stat.utils.FileUtils.moveNewlogFiles
+import com.zyuc.stat.utils.FileUtils.{downloadFileFromHdfs, moveNewlogFiles, renameHDFSDir}
 
-import scala.collection.mutable
 
 /**
   * Created by zhoucw on 17-7-19.
   */
 object MMELogETL extends Logging{
   def main(args: Array[String]): Unit = {
-    val sparkConf = new SparkConf()
+    val sparkConf = new SparkConf().setMaster("local[4]").setAppName("fd")
     val sc = new SparkContext(sparkConf)
     val sqlContext = new HiveContext(sc)
 
     val loadTime = "201717191945"
+    // val loadTime = sc.getConf.get("spark.app.loadTime")
+
+    // val inputPath = sc.getConf.get("spark.app.inputPath")
+    val inputPath = "hdfs://EPC-LOG-NM-15:8020/hadoop/IOT/ANALY_PLATFORM/MME/newlog"
+    val outputPath = "hdfs://EPC-LOG-NM-15:8020/hadoop/IOT/ANALY_PLATFORM/MME/"
 
     val fileSystem = FileSystem.get(sc.hadoopConfiguration)
 
-    val outputPath = "hdfs://EPC-LOG-NM-15:8020/hadoop/IOT/ANALY_PLATFORM/MME/"
+    // mme日志文件名的通配符
+    val hwmmWildcard =  sc.getConf.get("spark.app.HUmmWildcard")
+    val hwsmWildcard =  sc.getConf.get("spark.app.HUsmWildcard")
+    val ztmmWildcard =  sc.getConf.get("spark.app.ZTmmWildcard")
+    val ztsmWildcard =  sc.getConf.get("spark.app.ZTsmWildcard")
 
-    val filestatus = fileSystem.globStatus(new Path(outputPath + "newlog/" + "*.csv"))
+    // rename original dir
+    val srcLocation = inputPath + "/" + loadTime
+    val destLocation = inputPath + "/" + loadTime + "_doing"
+    renameHDFSDir(fileSystem, srcLocation, destLocation)
 
-    val outputDataPath = outputPath + "data/" + loadTime
 
-    moveNewlogFiles(outputPath, filestatus,loadTime)
 
+    //val filestatus = fileSystem.globStatus(new Path(outputPath + "newlog/" + "*.csv"))
+
+    //val outputDataPath = outputPath + "data/" + loadTime
+
+   // moveNewlogFiles(outputPath, filestatus,loadTime)
+    val hdfsDirLocation = "hdfs:/tmp/zcw/"
+    val localDirLocation = "/tmp/zcw1/"
+    val suffix = loadTime
+
+    downloadFileFromHdfs(fileSystem, hdfsDirLocation:String, localDirLocation:String, suffix:String)
 
   }
 
