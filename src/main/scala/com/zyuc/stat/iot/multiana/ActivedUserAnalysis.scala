a92f46e4a34a2a9d34439d765c918438f314aeb1
learnspark123 <421575422@qq.com>
2017-09-05 10:25:35 +0800

    [活跃用户]

--- src/main/scala/com/zyuc/stat/iot/multiana/ActivedUserAnalysis.scala
+++ src/main/scala/com/zyuc/stat/iot/multiana/ActivedUserAnalysis.scala
@@ -8,6 +8,7 @@ import org.apache.spark.sql.hive.HiveContext
 
 /**
   * Created by zhoucw on 17-8-17.
+  * modify by limm 2017/09/04
   */
 object ActivedUserAnalysis {
 
@@ -19,7 +20,9 @@ object ActivedUserAnalysis {
     sqlContext.sql("use " + hivedb)
 
     val appName =  sc.getConf.get("spark.app.name") //
-    val radiusTable = sc.getConf.get("spark.app.table.radiusTable")  //iot_radius_data_day
+    val pdsnTable = sc.getConf.get("spark.app.table.pdsn")  //iot_cdr_data_pdsn_d 3g
+    val pgwTable = sc.getConf.get("spark.app.table.pgw")  //iot_cdr_data_pgw_d   4g
+    val haccgTable = sc.getConf.get("spark.app.table.haccg")  //iot_cdr_data_haccg_d  3g
     val userTablePartitionID = sc.getConf.get("spark.app.userTablePartitionID")
     val userTable = sc.getConf.get("spark.app.table.userTable") //"iot_customer_userinfo"
     val activeUserTable = sc.getConf.get("spark.app.table.activeUserTable") //"iot_activeduser_data_day"
@@ -27,14 +30,27 @@ object ActivedUserAnalysis {
     val outputPath = sc.getConf.get("spark.app.outputPath")  //  "hdfs://EPC-IOT-ES-06:8020/hadoop/IOT/data/activeUser/secondaryoutput/"
 
 
-    val radiusPartitionID = dayid.substring(2,8)
-    val raidusTable = s"${appName}_tmp_raidus"
-    sqlContext.sql(
-      s"""CACHE TABLE ${raidusTable} as
-         |select mdn, nettype
-         |from ${radiusTable} t
-         |where d='${radiusPartitionID}'
+    val cdrPartitionID = dayid.substring(2,8)
+    //iot_cdr_data_pdsn_d 3g
+    val cdrTable = "tmp_cdr_table"
+    val cdrDF = sqlContext.sql(
+      s"""CACHE TABLE ${cdrTable} as
+         |select mdn, '3G' nettype
+         |from ${pdsnTable} t
+         |where d='${cdrPartitionID}'
+         |union all
+         |select mdn, '4G' nettype
+         |from ${pgwTable} t
+         |where d='${cdrPartitionID}'
        """.stripMargin)
+
+    //val raidusTable = s"${appName}_tmp_raidus"
+    //sqlContext.sql(
+    //  s"""CACHE TABLE ${raidusTable} as
+    //     |select mdn, nettype
+    //     |from ${radiusTable} t
+    //     |where d='${radiusPartitionID}'
+    //   """.stripMargin)
 
     val tmpUserTable = s"${appName}_tmp_user"
     sqlContext.sql(
@@ -47,18 +63,21 @@ object ActivedUserAnalysis {
 
     val activeUserSQL =
       s"""
-         |select '${radiusPartitionID}' as d, vpdncompanycode, '3G' nettype, count(*) as activednum
-         |from ${tmpUserTable} t left semi join ${raidusTable} r
+         |select '${cdrPartitionID}' as d, vpdncompanycode, '3G' nettype, count(*) as activednum
+         |from ${tmpUserTable} t left semi join ${cdrTable} r
          |on(t.mdn=r.mdn and r.nettype='3G')
          |group by vpdncompanycode
          |union all
-         |select '${radiusPartitionID}' as d, vpdncompanycode, '4G' nettype, count(*) as activednum
-         |from ${tmpUserTable} t left semi join ${raidusTable} r
+         |select '${cdrPartitionID}' as d, vpdncompanycode, '4G' nettype, count(*) as activednum
+         |from ${tmpUserTable} t left semi join ${cdrTable} r
          |on(t.mdn=r.mdn and r.nettype='4G')
          |group by vpdncompanycode
      """.stripMargin
 
     val resultDF = sqlContext.sql(activeUserSQL)
+
+
+
     val fileSystem = FileSystem.newInstance(sc.hadoopConfiguration)
     val coalesceNum = 1
     val partitions="d"
